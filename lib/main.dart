3fc3c08a1175b1a2607b4e9c354ee0de10875916
AdrianRudeanu <48693528+AdrianRudeanu@users.noreply.github.com>
2021-01-10 20:04:17 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_redux/flutter_redux.dart';
+import 'package:instagram_clone/src/init/init.dart';
+import 'package:instagram_clone/src/models/index.dart';
+import 'package:instagram_clone/src/presentation/routes.dart';
+import 'package:redux/redux.dart';
+
+void main() {
+  runApp(const InstagramClone());
+}
+
+class InstagramClone extends StatefulWidget {
+  const InstagramClone({Key key}) : super(key: key);
+
+  @override
+  _InstagramCloneState createState() => _InstagramCloneState();
+}
+
+class _InstagramCloneState extends State<InstagramClone> {
+  Future<Store<AppState>> _future;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _future = init();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder<Store<AppState>>(
+      builder: (BuildContext context, AsyncSnapshot<Store<AppState>> snapshot) {
+        if (snapshot.hasData) {
+          final Store<AppState> store = snapshot.data;
+
+          return StoreProvider<AppState>(
+            store: store,
+            child: MaterialApp(
+              title: 'Instagram clone',
+              theme: ThemeData.dark(),
+              routes: AppRoutes.routes,
+            ),
+          );
+        } else {
+          if (snapshot.hasError) {
+            throw snapshot.error;
+          }
+          return MaterialApp(
+            title: 'Instagram clone',
+            theme: ThemeData.dark(),
+            home: const Scaffold(
+              body: Center(
+                child: CircularProgressIndicator(),
+              ),
+            ),
+          );
+        }
+      },
+    );
+  }
+}
---
+++ lib/src/presentation/routes.dart
@@ -0,0 +1,16 @@
+import 'dart:js';
+
+import 'package:flutter/cupertino.dart';
+
+import 'home.dart';
+
+// ignore: avoid_classes_with_only_static_members
+class AppRoutes {
+  static const String home = '/';
+
+  static Map<String, WidgetBuilder> routes = <String, WidgetBuilder>{
+    home: (BuildContext context) {
+      return const Home();
+    }
+  };
+}
